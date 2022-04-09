60de8963c13c651c4282b77388946c71261fcb42
Oleksii Shevchenko <oleksiishevchenko93@gmail.com>
2022-04-09 13:26:31 +0300

    'test_3'

--- client/src/routes.js
+++ client/src/routes.js
@@ -12,7 +12,8 @@ export const useRoutes = (isAuthenticated) => {
         <Route path={'/api/notes'} element={<NotesPage />} />
         <Route path={'/api/users/me'} element={<ProfilePage />} />
         <Route path={'/api/notes/:id'} element={<DetailPage />} />
-        <Route path={'*'} element={<Navigate replace to="/api/notes" />} />
+        <Route path={'*'} element={<ProfilePage />} />
+        {/*<Route path={'*'} element={<Navigate replace to="/api/notes" />} />*/}
       </Routes>
     )
   }
