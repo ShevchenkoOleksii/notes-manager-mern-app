e50e99a1a9f2fbaecb53c4941c6fea0fed274d75
Oleksii Shevchenko <oleksiishevchenko93@gmail.com>
2022-08-09 13:17:23 +0300

    added another fb button v2

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -72,6 +72,30 @@ export const AuthPage = () => {
   return (
     <div className="row">
       <ShareFacebookButton />
+      <Helmet>
+     <title>{'title'}</title>
+     <meta charset="utf-8" />
+     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+     <meta name="csrf_token" content="" />
+     <meta property="type" content="website" />
+     <meta property="url" content={href} />
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+     <meta name="robots" content="noodp" />
+     <meta property="title" content={'title2'} />
+     <meta property="quote" content={"quote"} />
+     <meta name="description" content={"description"} />
+     <meta property="image" content={imageUrl} />
+     <meta property="og:locale" content="en_US" />
+     <meta property="og:type" content="website" />
+     <meta property="og:title" content={'title3'} />
+     <meta property="og:quote" content={"quote2"} />
+     <meta property="og:hashtag" content={"hashtag"} />
+     <meta property="og:image" content={imageUrl} />
+     <meta content="image/*" property="og:image:type" />
+     <meta property="og:url" content={href} />
+     <meta property="og:site_name" content="CampersTribe" />
+     <meta property="og:description" content={"description2"} />    
+     </Helmet>
       <FacebookShareButton 
                 url={href}
                 quote={"CampersTribe - World is yours to explore"}
